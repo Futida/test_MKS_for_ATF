0c1f3c23f14f628951d44338ef9134f71a7e4153
konstantin <mihrutka@inbox.ru>
2019-04-03 16:02:43 +0300

    Поправил роуты и переменную для базового урла

--- routes/routes.js
+++ routes/routes.js
@@ -3,49 +3,48 @@ const router = express.Router();
 // node-postgres
 const pg = require('pg');
 const fetch = require('node-fetch');
-
-// config postgres
-const configDB = require('../constants/configDB');
+// postgres://${username}:${password}@${localhost}/${database}
+const connectionString = process.env.DB_CONN_STRING || "postgres://atf_user:1q2w3e4r5t@10.2.9.96/atf_demo";
 // Connect to database
-const pool = new pg.Pool(configDB.config);
+const pool = new pg.Pool({ connectionString });
+
 
 let flag = true;
 
 router.route('/route1').post(function(req, res) {
-
     pool.connect(function(err, client, done) {
         if (err) {
             console.log("Can not connect to the DB" + err);
         }
-        client.query('UPDATE atf SET name = \'test1\' WHERE name = \'test\'', function(err, result) {
+        client.query('UPDATE atf SET name = \'atf_demo_1\' WHERE name = \'atf_demo\'', function(err, result) {
             done();
             if (err) {
                 console.log(err);
                 res.status(400).send(err);
             }
-            fetch('https://randomuser.me/api')
-            .then(res => res.json())
-            .then((json) => {
-                if (json.results) {
-                    res.status(200);
-                    res.json(json.results[0]);
+            fetch(`${process.env.API_BASE_URL}/1`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    data: 'atf_demo_1',
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
                 }
             })
+            .then(response => response.json())
+            .then(json => res.send(json))
         })
     })
 });
 
 router.route('/route2').post(function(req, res) {
-
     const header = req.get('x-atf');
 
     if (header) {
-        fetch('https://jsonplaceholder.typicode.com/posts', {
+        fetch(`${process.env.API_BASE_URL}/2`, {
             method: 'POST',
             body: JSON.stringify({
-                title: 'foo',
-                body: 'bar',
-                userId: 1
+                data: 'atf_demo_1',
             }),
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
@@ -56,19 +55,15 @@ router.route('/route2').post(function(req, res) {
     } else {
         res.status(400).send('Нет нужного заголовка');
     }
-
 });
 
 
 router.route('/route3').post(function(req, res) {
-
     if (res.statusCode === 200) {
-        fetch('https://jsonplaceholder.typicode.com/posts', {
+        fetch(`${process.env.API_BASE_URL}/3`, {
             method: 'POST',
             body: JSON.stringify({
-                title: 'foo',
-                body: 'bar',
-                userId: 1
+                data: 'atf_demo_1',
             }),
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
@@ -83,27 +78,22 @@ router.route('/route3').post(function(req, res) {
 });
 
 router.route('/route4').post(function(req, res) {
-
     if (flag) {
         flag = false;
         return res.json({ message: 'ok' });
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
+    fetch(`${process.env.API_BASE_URL}/4`, {
         method: 'POST',
         body: JSON.stringify({
-            title: 'foo',
-            body: 'bar',
-            userId: 1
+            data: 'atf_demo_1',
         }),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
     })
     .then(response => response.json())
-    .then(json => {
-        res.send(json);
-    });
+    .then(json => res.send(json));
 
 });
 
