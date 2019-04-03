175bcfc1fa3c61aa770864c6dfb7db60f62b82bf
konstantin <mihrutka@inbox.ru>
2019-04-03 12:29:36 +0300

    Изменил настройки БД

--- constants/configDB.js
+++ constants/configDB.js
@@ -1,11 +1,11 @@
 const config = {
-    user: 'admin',
-    database: 'test',
-    password: 'admin',
+    user: 'atf_user',
+    database: 'atf_demo',
+    password: '1q2w3e4r5t',
     port: 5432
 };
 // postgres://${username}:${password}@${localhost}/${database}
-const connectionString = "postgres://admin:admin@localhost:5432/test";
+const connectionString = "postgres://admin:admin@10.2.9.96/atf_demo";
 
 module.exports = {
     config: config,
