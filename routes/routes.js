34c03ebf3c5903bdca3dff88bf2a6827d65c50af
konstantin <mihrutka@inbox.ru>
2019-04-08 16:16:38 +0300

    внес изменения по последним правкам

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,7 @@ const fetch = require('node-fetch');
 const connectionString = process.env.DB_CONN_STRING || "postgres://atf_user:1q2w3e4r5t@10.2.9.96/atf_demo";
 // Connect to database
 const pool = new pg.Pool({ connectionString });
+let flag = true;
 
 function createHeaders(r) {
     const { headers } = r;
@@ -26,12 +27,15 @@ function createHeaders(r) {
 
 router.route('/route1').post(function(req, res) {
     const headers = createHeaders(req);
-    
+    let { user, status } = req.body;
+
+    status = status === 'a' ? 'ACTIVE' : status === 'b' ? 'BLOCKED' : '';
+
     pool.connect(function(err, client, done) {
         if (err) {
             console.log("Can not connect to the DB" + err);
         }
-        client.query('UPDATE atf SET name = \'atf_demo_1\' WHERE name = \'atf_demo\'', function(err, result) {
+        client.query(`UPDATE atf SET status = ${status} WHERE user = ${user}`, function(err, result) {
             done();
             if (err) {
                 console.log(err);
@@ -40,31 +44,47 @@ router.route('/route1').post(function(req, res) {
             fetch(`${process.env.API_BASE_URL}/1`, {
                 method: 'POST',
                 body: JSON.stringify({
-                    data: 'atf_demo_1',
+                    user: "demo_user",
+                    status: "b",
                 }),
                 headers
             })
             .then(response => response.json())
-            .then(json => res.send(json))
+            .then(() => {
+                res.status(200).send('');
+            })
         })
     })
 });
 
 router.route('/route2').post(function(req, res) {
-    const header = req.get('x-atf');
-    const headers = createHeaders(req);
-    req.body["request_id"] = Math.floor(Math.random() * 10000000);
+    const headers = req.headers;
+    const key = Object.keys(headers).filter(header => header === 'x-atf');
 
-    if (header) {
-        fetch(`${process.env.API_BASE_URL}/2`, {
-            method: 'POST',
-            body: JSON.stringify(req.body),
-            headers
-        })
-        .then(response => response.json())
-        .then(json => res.send(json))
+    if (key && !key.length) {
+        res.status(400).send({
+            errorCode: "LOGIC_ERROR",
+            cause: "Отсутствует заголовок x-atf"
+        });
     } else {
-        res.status(400).send('Нет нужного заголовка');
+        const header = req.get('x-atf');
+        const headers = createHeaders(req);
+        req.body["request_id"] = Math.floor(Math.random() * 10000000);
+
+        if (header === '1223') {
+            fetch(`${process.env.API_BASE_URL}/2`, {
+                method: 'POST',
+                body: JSON.stringify(req.body),
+                headers
+            })
+            .then(response => response.json())
+            .then(json => res.send(json))
+        } else {
+            res.status(400).send({
+                errorCode: "LOGIC_ERROR",
+                cause: "Значение заголовка x-atf некорректно",
+            });
+        }
     }
 });
 
@@ -75,7 +95,8 @@ router.route('/route3').post(function(req) {
     fetch(`${process.env.API_BASE_URL}/1`, {
         method: 'POST',
         body: JSON.stringify({
-            data: 'atf_demo_1',
+            user: "demo_user",
+            status: "b"
         }),
         headers
     })
@@ -85,7 +106,9 @@ router.route('/route3').post(function(req) {
     fetch(`${process.env.API_BASE_URL}/2`, {
         method: 'POST',
         body: JSON.stringify({
-            data: 'atf_demo_2',
+            id: "25",
+            user: "demo_user",
+            request_id: "123456"
         }),
         headers
     })
@@ -107,44 +130,69 @@ router.route('/route3').post(function(req) {
 
 router.route('/route4').post(function(req, res) {
     const headers = createHeaders(req);
-
-    fetch(`${process.env.API_BASE_URL}/4`, {
-        method: 'POST',
-        body: JSON.stringify({
-            data: 'atf_demo_4',
-        }),
-        headers
-    })
-    .then(response => response.json())
-    .then(json => {
-        pool.connect(function(err, client, done) {
-            if (err) {
-                console.log("Can not connect to the DB" + err);
-            }
-            client.query(`INSERT INTO atf VALUES (4, ${json.data} )`, function(err) {
-                done();
-                if (err) {
-                    console.log(err);
-                    res.status(400).send(err);
-                }
-            })
-        })
-    });
+    const { user, account_id } = req.body;
+    const currentTime = Math.round(new Date().getTime() / 1000);
 
     pool.connect(function(err, client, done) {
         if (err) {
             console.log("Can not connect to the DB" + err);
         }
-        client.query('SELECT id,name FROM atf ORDER BY id DESC LIMIT 1', function(err, result) {
+        client.query(`SELECT time FROM atf WHERE account_id=${account_id}`, function(err, result) {
             done();
             if (err) {
                 console.log(err);
                 res.status(400).send(err);
             }
+            const timeFromTable = (result.rows[0] / 1000);
 
+            if (currentTime - timeFromTable > process.env.CACHETIME) {
+                flag = true
+            }
+        })
+    });
+
+    if (flag) {
+        fetch(`${process.env.API_BASE_URL}/4`, {
+            method: 'POST',
+            body: JSON.stringify({
+                user,
+                account_id
+            }),
+            headers
+        })
+        .then(response => response.json())
+        .then((json) => {
+            pool.connect(function(err, client, done) {
+                if (err) {
+                    console.log("Can not connect to the DB" + err);
+                }
+                client.query(`UPDATE atf SET data = ${json.data}, time=${new Date().getTime()} WHERE account_id=${account_id}`, function(err) {
+                    done();
+                    if (err) {
+                        console.log(err);
+                        res.status(400).send(err);
+                    } else {
+                        flag = false;
+                    }
+
+                })
+            })
+        });
+    }
+
+    pool.connect(function(err, client, done) {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        }
+        client.query(`SELECT user as user, account_id, params, data FROM atf WHERE account_id=${account_id}`, function(err, result) {
+            done();
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
             res.send(result.rows[0]);
         })
-    })
+    });
 });
 
 module.exports = router;
