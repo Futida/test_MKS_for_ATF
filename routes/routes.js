135726833dbf50d8d16f4e2e2e30e73ed455c3b7
konstantin <mihrutka@inbox.ru>
2019-04-16 15:50:05 +0300

    правки

--- routes/routes.js
+++ routes/routes.js
@@ -76,7 +76,9 @@ router.route('/route2').post(function(req, res) {
                 body: JSON.stringify(req.body),
                 headers
             })
-            .then(response => res.send(response))
+            .then(response => response.json())
+            .then(json => res.send(json))
+            .catch(err => console.log('>>>>>>>>>>>>>>>>>>>>>', err))
         } else {
             res.status(400).send({
                 errorCode: "LOGIC_ERROR",
